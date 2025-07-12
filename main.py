9d9434054a130a298446faae05b42e3ad0f971b2
egrks <egrksnew@yandex.ru>
2025-07-12 14:27:08 +0200

    shapes

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 from constants import *
+from player import *
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player_one = Player(x,y)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,9 +18,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000000)
+        player_one.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60)/1000
+        
+        
         
 
 if __name__ == "__main__":
